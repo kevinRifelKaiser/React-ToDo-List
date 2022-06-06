1604d4d03594b4c6715da2c9487e849760b267dd
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-06-06 20:42:58 -0300

    Add delete option and an explanation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,17 @@ import React, {useState} from "react";
 import ToDoItems from "./ToDoItems";
 
 function App() {
-
+  //1. inputText almacenara el valor de lo que se escribe en el input
   const [inputText, setInputText] = useState('');
+  //2. items almacenara el valor final del inputText
   const [items, setItems] = useState([]);
 
+  //1.1 esta función ira almacenando lo que se escriba en el input a medida que su valor cambie (ver 1.1.1 donde se targetea esta función)
   function handleInputText(event) {
     const newValue = event.target.value;
     setInputText(newValue);    
   }
-
+  //2.1 esta función alamacena en el array los valores a medida que se aprite "add" y vaciamos lo que este escrito en el input luego de almacenarlo (la usamos en 2.1.1).
   function handleItems() {
     setItems(prevValue=> {
       return[
@@ -19,7 +21,15 @@ function App() {
       ]
   });
     setInputText('');
-  } 
+  }
+  //3. esta función la usamos para eliminar el item al que le hagamos click. Para esto en 3.1 añadimos al método map un index ya que nos da la posibilidad, y le damos al list item un id igual al index para targetearlo.
+  function deleteItem(id) {
+    setItems((prevItems)=> {
+      return prevItems.filter((item, index)=>{
+        return index !== id;
+      })
+    });
+  }
 
   return (
     <div className="container">
@@ -27,20 +37,22 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input 
+        <input
+        //1.1.1 handleInputText
         onChange={handleInputText} 
         type="text"
-        name="newEntry"
         value={inputText}
         />
+        {/* 2.1.1 handleItems */}
         <button onClick={handleItems}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-        {items.map((todoItem) => (
-            <ToDoItems text={todoItem} />
+        {/*3.1. además usamos el index para darle la key al list item, ya que todos los itmes tienen que tener una key, en este caso lo hacemos igual al index aunque no es conveniente, deberíamos crear un campo especial para esto, podemos usar el paquete uuid por ejemplo. Luego en el componente "ToDoItems,jsx", en el punto 3.1, accedemos a este componente que lo contiene, a travez de la propiedad id para desde una jerarquía mas abajo, activar la función de delete.*/}
+        {items.map((todoItem, index) => (
+            <ToDoItems key={index} id={index} text={todoItem} onChecked={deleteItem} />
           ))}
         </ul>
       </div>
--- src/components/ToDoItems.jsx
+++ src/components/ToDoItems.jsx
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ToDoItems(props) {
-  const [click, setClick] = useState(false);
-
-  function handleClick() {
-    setClick((prevValue) => {
-      return !prevValue;
-    });
-  }
 
   return (
-    <div onClick={handleClick}>
-      <li style={{ textDecoration: click ? "line-through" : "none" }}>
-        {props.text}
-      </li>
+    //3.2. Acá con la propiedad onClick, podemos acceder a las propiedades del list item para que haga algo cuando le hacemos click. En este caso accederá a la propiedad onChecked que creamos en "App.jsx" (la cual llama a la función deleteItem) y le pasamos como parámetro el id del item al que le hacemos click, para que a su vez se lo pase a la función y esta filtre el array y devbuelva todos los items menos el que le hicimos click.
+    //Notese que a la propiedad onClick no le pasamos directamente props.onChecked(props.id), si no que se lo pasamos dentro de una función, esto es porque, dado como tenemos construida nuestra aplicación, si no lo pasamos con una función, cuando agreguemos un items con el boton add, este renderizará el list item llamando a la propieda onChecked haciendo que crashee, metiendolo en una función hacemos que se active únicamente cuando le hacemos click al list item que queremos eliminar.
+    <div onClick={()=>{
+      props.onChecked(props.id)
+      }}>
+      <li>{props.text}</li>
     </div>
   );
+  
 }
 
 export default ToDoItems;
